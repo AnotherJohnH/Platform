71c9d87daaab390a18406cf095708b4377b1c4ce
John D. Haughton <anotherjohnh@gmail.com>
2017-11-04 21:28:36 +0000

    Add missing Pinx.h stub

---
+++ Source/MTL/chip/stub/Pins.h
@@ -0,0 +1,38 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2017 John D. Haughton
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
+
+//! \file Pin.h
+//! \brief
+
+#ifndef MTL_PINS_H
+#define MTL_PINS_H
+
+namespace MTL {
+
+static const unsigned PIN_LED1 = 1;
+static const unsigned PIN_LED2 = 2;
+static const unsigned PIN_LED3 = 3;
+static const unsigned PIN_LED4 = 4;
+
+} // namespace MTL
+
+#endif // MTL_PINS_H
