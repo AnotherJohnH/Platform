6bbf3f0fe6fd5dd973ce1ea721a9d025a4688b10
John D. Haughton <anotherjohnh@gmail.com>
2017-11-20 21:55:18 +0000

    tinyc header updates

--- include/MTL/tinyc/ctype.h
+++ include/MTL/tinyc/ctype.h
@@ -20,11 +20,11 @@
 // SOFTWARE.
 //----------------------------------------------------------------------------*/
 
-//! \file string.h
+//! \file ctype.h
 //! \brief tiny C library implementation
 
-#ifndef STRING_H
-#define STRING_H
+#ifndef CTYPE_H
+#define CTYPE_H
 
 #include <stdlib.h>
 
@@ -32,21 +32,14 @@
 extern "C" {
 #endif
 
-extern char*  strcpy(char*, const char*);
-extern char*  strncpy(char*, const char*, size_t);
-extern char*  strcat(char*, const char*);
-extern int    strcmp(const char*, const char*);
-extern int    strncmp(const char*, const char*, size_t);
-extern size_t strlen(const char*);
-
-extern char*  strchr(const char*, int);
-extern char*  strrchr(const char*, int);
-
-extern void*  memcpy(void*, const void*, size_t);
-extern void*  memset(void*, int, size_t);
+extern int isdigit(int c);
+extern int isalpha(int c);
+extern int islower(int c);
+extern int isupper(int c);
+extern int isalnum(int c);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif // STRING_H
+#endif // CTYPE_H
