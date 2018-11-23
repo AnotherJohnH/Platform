0c63310d65e81b1df73f3a0fe864ae5800a9fb34
John D. Haughton <anotherjohnh@gmail.com>
2018-11-23 21:13:24 +0000

    Rework STB/IFF to be more useful

--- include/STB/IFF.h
+++ include/STB/IFF.h
@@ -27,6 +27,8 @@
 #include <cstdio>
 #include <cstring>
 
+#include <vector>
+
 #include "STB/Endian.h"
 
 namespace STB {
@@ -44,7 +46,7 @@ public:
       return fread(this, sizeof(TYPE), 1, fp) == 1;
    }
 
-   bool write(FILE* fp)
+   bool write(FILE* fp) const
    {
       return fwrite(this, sizeof(TYPE), 1, fp) == 1;
    }
@@ -71,11 +73,6 @@ public:
    {
       return !operator==(rhs);
    }
-
-   bool match(FILE* fp, const char* expected)
-   {
-      return read(fp) && operator==(expected);
-   }
 };
 
 //! IFF 32-bit unsigned integer
@@ -91,48 +88,248 @@ public:
    }
 
    operator uint32_t() const { return value; }
+
+   uint32_t operator=(uint32_t value_) { value = value_; return value_; }
 };
 
-//! IFF chunk header
-struct ChunkHeader : public FileIo<ChunkHeader>
-{
-   Ident  ident;
-   UInt32 size;
-
-   ChunkHeader(const char* ident_ = "    ", uint32_t size_ = 0)
-      : ident(ident_)
-      , size(size_)
-   {
-   }
-
-   bool match(FILE* fp, const char* expected)
-   {
-      return read(fp) && (ident == expected);
-   }
-};
-
-//! IFF document
-class Form
+//! IFF chunk
+class Chunk
 {
 public:
-   Form(const char* type_ = "    ")
-      : file_type(type_)
+   Chunk(const char* type_, size_t reserve_ = 0)
+      : type(type_)
    {
+      bytes.reserve(reserve_);
    }
 
+   //! Get chunk type
+   const Ident& getType() const { return type; }
+
+   //! Get chunk size
+   uint32_t getSize() const { return size; }
+
+   //! Get total chunk file size
+   size_t getFileSize() const
+   {
+      size_t file_size = size;
+      // Ensure size is an even number
+      if (file_size & 1) file_size++;
+      return file_size + 8;
+   }
+
+   //! Get chunk data
+   const void* getData() const { return bytes.data(); }
+
+   //! Allocate chunk data
+   void* alloc()
+   {
+      bytes.resize(size);
+      return bytes.data();
+   }
+
+   //! Read chunk header and skip to next chunk
    bool read(FILE* fp)
    {
-      return file_header.match(fp, "FORM") && file_type.read(fp);
+      bool ok = false;
+
+      if (type.read(fp) && size.read(fp))
+      {
+         ok = fseek(fp, size, SEEK_CUR) == 0;
+      }
+
+      return ok;
    }
 
-   bool match(FILE* fp, const char* expected)
+   //! Write chunk
+   bool write(FILE* fp) const
    {
-      return read(fp) && (file_type == expected);
+      type.write(fp);
+      size.write(fp);
+
+      if (!bytes.empty())
+      {
+         if (fwrite(bytes.data(), bytes.size(), 1, fp) != 1) return false;
+
+         if (bytes.size() & 1)
+         {
+            const uint8_t zero = 0;
+            if (fwrite(&zero, 1, 1, fp) != 1) return false;
+         }
+      }
+
+      return true;
+   }
+
+   //! Add raw data to chunk
+   void push(const void* data_ptr, size_t n)
+   {
+      size_t end = bytes.size();
+      bytes.resize(end + n);
+      size = bytes.size();
+      memcpy(&bytes[end], data_ptr, n);
+   }
+
+   //! Add object to chunk
+   template <typename TYPE>
+   void push(const TYPE& data)
+   {
+      push(&data, sizeof(TYPE));
    }
 
 private:
-   ChunkHeader file_header{"FORM"};
-   Ident       file_type;
+   Ident                 type;
+   UInt32                size;
+   std::vector<uint8_t>  bytes;
+};
+
+
+//! IFF document
+class Document
+{
+public:
+   Document() = default;
+
+   Document(const char* document_type_, const char* file_type_)
+      : document_type(document_type_)
+      , file_type(file_type_)
+   {
+   }
+
+   ~Document()
+   {
+      if (fp != nullptr) fclose(fp);
+   }
+
+   //! Test document type
+   bool isDocType(const char* type) const { return document_type == type; }
+
+   //! Test file type
+   bool isFileType(const char* type) const { return file_type == type; }
+
+   //! Add a new chunk
+   Chunk& addChunk(const char* type, size_t reserve = 0)
+   {
+      chunk_list.emplace_back(type, reserve);
+      return chunk_list.back();
+   }
+
+   //! Find a chunk by name
+   Chunk* findChunk(uint32_t offset_)
+   {
+      size_t offset = 12;
+
+      for(auto& chunk : chunk_list)
+      {
+         if (offset == offset_)
+         {
+            return &chunk;
+         }
+
+         offset += chunk.getFileSize();
+      }
+
+      return nullptr;
+   }
+
+   //! Read a document
+   bool read(const char* filename)
+   {
+      if (!open(filename, "r")) return false;
+
+      bool ok = false;
+
+      if (document_type.read(fp) &&
+          file_size.read(fp) &&
+          file_type.read(fp))
+      {
+         ok = true;
+
+         size_t size = 4;
+
+         while(size < file_size)
+         {
+            Chunk& chunk = addChunk("    ");
+            if (!chunk.read(fp))
+            {
+               ok = false;
+               break;
+            }
+
+            size += chunk.getFileSize();
+         }
+      }
+
+      return ok;
+   }
+
+   //! Write a document
+   bool write(const char* filename)
+   {
+      if (!open(filename, "w")) return false;
+
+      size_t size = 4;
+      for(const auto& chunk : chunk_list)
+      {
+         size += chunk.getFileSize();
+      }
+      file_size = uint32_t(size);
+
+      bool ok = false;
+
+      if (document_type.write(fp) &&
+          file_size.write(fp) &&
+          file_type.write(fp))
+      {
+         ok = true;
+
+         for(const auto& chunk : chunk_list)
+         {
+            if(!chunk.write(fp))
+            {
+               ok = false;
+               break;
+            }
+         }
+      }
+
+      return ok;
+   }
+
+   //! Load the named chunk
+   template <typename TYPE>
+   const TYPE* load(const char* type)
+   {
+      size_t offset = 12;
+
+      for(auto& chunk : chunk_list)
+      {
+         if (chunk.getType() == type)
+         {
+            void* data = chunk.alloc();
+            fseek(fp, offset + 8, SEEK_SET);
+            fread(data, chunk.getSize(), 1, fp);
+            return static_cast<TYPE*>(data);
+         }
+
+         offset += chunk.getFileSize();
+      }
+
+      return nullptr;
+   }
+
+private:
+   FILE*              fp{nullptr};
+   Ident              document_type{};
+   UInt32             file_size{0};
+   Ident              file_type{};
+   std::vector<Chunk> chunk_list;
+
+   bool open(const char* filename, const char* mode)
+   {
+      if (fp != nullptr) fclose(fp);
+      fp = fopen(filename, mode);
+      return fp != nullptr;
+   }
 };
 
 } // namespace IFF
