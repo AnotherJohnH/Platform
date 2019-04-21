d31d8fe247eb86a9b1ac61899217fc0395536beb
John D. Haughton <anotherjohnh@gmail.com>
2019-04-21 19:16:33 +0100

    Tidy up Kindle3 frame buffer access

--- Source/PLT/Kindle3/Frame.cpp
+++ Source/PLT/Kindle3/Frame.cpp
@@ -33,6 +33,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <algorithm>
+
 #include "PLT/Frame.h"
 
 namespace PLT {
@@ -40,41 +42,28 @@ namespace PLT {
 class Frame::Impl
 {
 private:
-   unsigned width;
-   unsigned height;
-   uint8_t* buffer;
-   int      refresh_fd;
+   unsigned width{600};
+   unsigned height{800};
+   unsigned pitch{300};
+   uint8_t* buffer{nullptr};
+   int      refresh_fd{-1};
 
-   size_t getSize() const { return width * height / 2; }
+   size_t getSize() const { return pitch * height; }
 
-   static void error(const char* format, ...)
+   static void error(const char* message)
    {
-      va_list ap;
-
-      fprintf(stderr, "ERROR - ");
-
-      va_start(ap, format);
-      vfprintf(stderr, format, ap);
-      va_end(ap);
-
-      fprintf(stderr, "\n");
+      fprintf(stderr, "ERROR - %s\n", message);
       exit(1);
    }
 
-   static int openDev(const char* filename)
+public:
+   Impl()
    {
-      int fd = open(filename, O_RDWR);
+      int fd = open("/dev/fb0", O_RDWR);
       if(-1 == fd)
       {
-         error("Failed to open \"%s\"", filename);
+         error("Failed to open \"/dev/fb0\"");
       }
-      return fd;
-   }
-
-public:
-   Impl(const char* title_, unsigned width_, unsigned height_, uint32_t flags_)
-   {
-      int fd = openDev("/dev/fb0");
 
       struct fb_var_screeninfo screeninfo;
       int                      status = ioctl(fd, FBIOGET_VSCREENINFO, &screeninfo);
@@ -85,9 +74,10 @@ public:
 
       width  = screeninfo.xres;
       height = screeninfo.yres;
+      pitch  = (width + 1)/2;
 
-      buffer = (uint8_t*)mmap(0, getSize(), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-      if(0 == buffer)
+      buffer = (uint8_t*)mmap(nullptr, getSize(), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+      if(nullptr == buffer)
       {
          error("mmap() failed");
       }
@@ -103,54 +93,47 @@ public:
       close(refresh_fd);
    }
 
-   uint8_t* getStorage(unsigned& pitch)
+   unsigned getWidth() const { return width; }
+
+   unsigned getHeight() const { return height; }
+
+   uint8_t* getStorage(unsigned& pitch_) const
    {
-      pitch = (width + 1)/2;
+      pitch_ = pitch;
       return buffer;
    }
 
-   void resize(unsigned width_, unsigned height_) {}
-
-   void refresh()
+   void refresh() const
    {
       (void)write(refresh_fd, "2", 1);
    }
 };
 
 
-Frame::Frame(const char* title_, unsigned width_, unsigned height_, uint32_t flags_)
-   : Image(width_, height_)
+Frame::Frame(const char* /* title */, unsigned width_, unsigned height_, uint32_t /* flags */)
 {
-   pimpl  = new Impl(title_, width, height, flags_);
+   pimpl  = new Impl();
    buffer = pimpl->getStorage(pitch);
+
+   resize(width_, height_);
 }
 
 Frame::~Frame() { delete pimpl; }
 
-void* Frame::getHandle() const
-{
-   return nullptr;
-}
-
-void Frame::setTitle(const char* title_)
-{
-}
-
 void Frame::resize(unsigned width_, unsigned height_)
 {
-   if((width == width_) && (height == height_)) return;
-
-   width  = width_;
-   height = height_;
-
-   pimpl->resize(width_, height_);
-   buffer = pimpl->getStorage(pitch);
+   width  = std::min(width_,  impl->getWidth());
+   height = std::min(height_, impl->getHeight());
 }
 
 void Frame::refresh() { pimpl->refresh(); }
 
-void Frame::setScanner(Scanner* scanner_)
-{
-}
+// Some stubs for unimplemented functionality
+
+void* Frame::getHandle() const { return nullptr; }
+
+void Frame::setTitle(const char* title_) {}
+
+void Frame::setScanner(Scanner* scanner_) {}
 
 } // namespace PLT
