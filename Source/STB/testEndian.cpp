2d12d2b441884d0911b5485ba0893c1f418e49fb
John D. Haughton <anotherjohnh@gmail.com>
2019-07-13 17:27:44 +0100

    Start switch to cmake

--- Source/STB/testEndian.cpp
+++ Source/STB/testEndian.cpp
@@ -20,75 +20,134 @@
 // SOFTWARE.
 //------------------------------------------------------------------------------
 
-
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-
 #include "STB/Endian.h"
 
+#include "gtest/gtest.h"
 
-template <typename T, typename U> void checkEQ(T value, U exp, const char* file, unsigned line)
+TEST(Endian, Big16)
 {
-   if(value != T(exp))
-   {
-      fprintf(stderr, "FAILED: %s:%u expected %u got %u\n", file, line, exp, value);
-      abort();
-   }
+   STB::Big16     big16;
+   const uint8_t* byte = (const uint8_t*)&big16;
+
+   big16 = 0x1234;
+
+   EXPECT_EQ(byte[0], 0x12);
+   EXPECT_EQ(byte[1], 0x34);
+
+   big16 = STB::endianSwap(big16);
+
+   EXPECT_EQ(byte[0], 0x34);
+   EXPECT_EQ(byte[1], 0x12);
 }
 
-
-#define EQ(A, B) checkEQ(A, B, __FILE__, __LINE__)
-
-
-void testBig32()
+TEST(Endian, Big32)
 {
    STB::Big32     big32;
    const uint8_t* byte = (const uint8_t*)&big32;
 
    big32 = 0x12345678;
 
-   EQ(byte[0], 0x12);
-   EQ(byte[1], 0x34);
-   EQ(byte[2], 0x56);
-   EQ(byte[3], 0x78);
+   EXPECT_EQ(byte[0], 0x12);
+   EXPECT_EQ(byte[1], 0x34);
+   EXPECT_EQ(byte[2], 0x56);
+   EXPECT_EQ(byte[3], 0x78);
 
    big32 = STB::endianSwap(big32);
 
-   EQ(byte[0], 0x78);
-   EQ(byte[1], 0x56);
-   EQ(byte[2], 0x34);
-   EQ(byte[3], 0x12);
+   EXPECT_EQ(byte[0], 0x78);
+   EXPECT_EQ(byte[1], 0x56);
+   EXPECT_EQ(byte[2], 0x34);
+   EXPECT_EQ(byte[3], 0x12);
 }
 
+TEST(Endian, Big64)
+{
+   STB::Big64     big64;
+   const uint8_t* byte = (const uint8_t*)&big64;
 
-void testLtl32()
+   big64 = 0x0123456789ABCDEF;
+
+   EXPECT_EQ(byte[0], 0x01);
+   EXPECT_EQ(byte[1], 0x23);
+   EXPECT_EQ(byte[2], 0x45);
+   EXPECT_EQ(byte[3], 0x67);
+   EXPECT_EQ(byte[4], 0x89);
+   EXPECT_EQ(byte[5], 0xAB);
+   EXPECT_EQ(byte[6], 0xCD);
+   EXPECT_EQ(byte[7], 0xEF);
+
+   big64 = STB::endianSwap(big64);
+
+   EXPECT_EQ(byte[0], 0xEF);
+   EXPECT_EQ(byte[1], 0xCD);
+   EXPECT_EQ(byte[2], 0xAB);
+   EXPECT_EQ(byte[3], 0x89);
+   EXPECT_EQ(byte[4], 0x67);
+   EXPECT_EQ(byte[5], 0x45);
+   EXPECT_EQ(byte[6], 0x23);
+   EXPECT_EQ(byte[7], 0x01);
+}
+
+TEST(Endian, Ltl16)
+{
+   STB::Ltl16     ltl16;
+   const uint8_t* byte = (const uint8_t*)&ltl16;
+
+   ltl16 = 0x1234;
+
+   EXPECT_EQ(byte[0], 0x34);
+   EXPECT_EQ(byte[1], 0x12);
+
+   ltl16 = STB::endianSwap(ltl16);
+
+   EXPECT_EQ(byte[0], 0x12);
+   EXPECT_EQ(byte[1], 0x34);
+}
+
+TEST(Endian, Ltl32)
 {
    STB::Ltl32     ltl32;
    const uint8_t* byte = (const uint8_t*)&ltl32;
 
    ltl32 = 0x12345678;
 
-   EQ(byte[0], 0x78);
-   EQ(byte[1], 0x56);
-   EQ(byte[2], 0x34);
-   EQ(byte[3], 0x12);
+   EXPECT_EQ(byte[0], 0x78);
+   EXPECT_EQ(byte[1], 0x56);
+   EXPECT_EQ(byte[2], 0x34);
+   EXPECT_EQ(byte[3], 0x12);
 
    ltl32 = STB::endianSwap(ltl32);
 
-   EQ(byte[0], 0x12);
-   EQ(byte[1], 0x34);
-   EQ(byte[2], 0x56);
-   EQ(byte[3], 0x78);
+   EXPECT_EQ(byte[0], 0x12);
+   EXPECT_EQ(byte[1], 0x34);
+   EXPECT_EQ(byte[2], 0x56);
+   EXPECT_EQ(byte[3], 0x78);
 }
 
-
-int main(int argc, const char* argv[])
+TEST(Endian, Ltl64)
 {
-   testBig32();
-   testLtl32();
+   STB::Ltl64     ltl64;
+   const uint8_t* byte = (const uint8_t*)&ltl64;
 
-   printf("OK\n");
+   ltl64 = 0x0123456789ABCDEF;
 
-   return 0;
+   EXPECT_EQ(byte[0], 0xEF);
+   EXPECT_EQ(byte[1], 0xCD);
+   EXPECT_EQ(byte[2], 0xAB);
+   EXPECT_EQ(byte[3], 0x89);
+   EXPECT_EQ(byte[4], 0x67);
+   EXPECT_EQ(byte[5], 0x45);
+   EXPECT_EQ(byte[6], 0x23);
+   EXPECT_EQ(byte[7], 0x01);
+
+   ltl64 = STB::endianSwap(ltl64);
+
+   EXPECT_EQ(byte[0], 0x01);
+   EXPECT_EQ(byte[1], 0x23);
+   EXPECT_EQ(byte[2], 0x45);
+   EXPECT_EQ(byte[3], 0x67);
+   EXPECT_EQ(byte[4], 0x89);
+   EXPECT_EQ(byte[5], 0xAB);
+   EXPECT_EQ(byte[6], 0xCD);
+   EXPECT_EQ(byte[7], 0xEF);
 }
