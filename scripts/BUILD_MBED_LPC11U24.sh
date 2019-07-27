acd17460581b1a106a0132362a97bac6bde5cd53
John D. Haughton <anotherjohnh@gmail.com>
2019-07-27 13:30:55 +0100

    Remove some old scons files

--- scripts/BUILD_MBED_LPC11U24.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/echo "source this file then type 'scons' to run SConstruct"
-#
-# Setup environment for MBED LPC11U24 build
-
-export PROJ_TARGET=mbedLPC11U24
-
--- scripts/BUILD_MBED_LPC1768.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/echo "source this file then type 'scons' to run SConstruct"
-#
-# Setup environment for MBED LPC1768 build
-
-export PROJ_TARGET=mbedLPC1768
-
