a4325eecd16e004733aaf4af5dbc08777f760821
John D. Haughton <anotherjohnh@gmail.com>
2019-10-21 22:15:44 +0100

    Tidy some doxygen comments

--- include/PHY/Const.h
+++ include/PHY/Const.h
@@ -27,16 +27,16 @@ namespace PHY {
 
 namespace Const {
 
-static const double c  = 299792458;        // Speed of light in a vacuum  [m/s]
-static const double L  = 6.022045E23;      // Avogadro constant           [/mol]
-static const double e  = 1.6021892E-19;    // Elementary charge           [C]
-static const double kb = 1.3806485279E-23; // Boltzmann constant          [J/K]
-static const double g  = 9.80665;          // standard gravity            [m/s^2]
-static const double G  = 6.6720E-11;       // Gravitational constant      [N.m^2/kg^2]
-static const double h  = 6.626176E-34;     // Planck constant             [J/Hz]
-static const double mp = 1.6726485e-27;    // Mass of proton              [kg]
-static const double mn = 1.6749543e-27;    // Mass of neutron             [kg]
-static const double me = 9.109534e-31;     // Mass of electron            [kg]
+static const double c  = 299792458;        //!< Speed of light in a vacuum [m/s]
+static const double L  = 6.022045e23;      //!< Avogadro constant          [/mol]
+static const double e  = 1.6021892e-19;    //!< Elementary charge          [C]
+static const double kb = 1.3806485279e-23; //!< Boltzmann constant         [J/K]
+static const double g  = 9.80665;          //!< standard gravity           [m/s^2]
+static const double G  = 6.6720e-11;       //!< Gravitational constant     [N.m^2/kg^2]
+static const double h  = 6.626176e-34;     //!< Planck constant            [J/Hz]
+static const double mp = 1.6726485e-27;    //!< Mass of proton             [kg]
+static const double mn = 1.6749543e-27;    //!< Mass of neutron            [kg]
+static const double me = 9.109534e-31;     //!< Mass of electron           [kg]
 
 } // Const
 
