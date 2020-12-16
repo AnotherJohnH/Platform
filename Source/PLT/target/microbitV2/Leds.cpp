24db96016b120dfa06da41ad2fa41ec86ecdc2c5
John D. Haughton <anotherjohnh@gmail.com>
2020-12-16 21:00:37 +0000

    Prepare for microbit V2 support

---
+++ Source/PLT/target/microbitV2/Leds.cpp
@@ -0,0 +1,32 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2020 John D. Haughton
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//------------------------------------------------------------------------------
+
+#include "MTL/Leds.h"
+
+const uint8_t MTL::Leds::map[5][5] =
+{
+   { 0x00, 0x13, 0x01, 0x14, 0x02 },
+   { 0x23, 0x24, 0x25, 0x26, 0x27 },
+   { 0x11, 0x08, 0x12, 0x28, 0x10 },
+   { 0x07, 0x06, 0x05, 0x04, 0x03 },
+   { 0x22, 0x16, 0x20, 0x15, 0x21 }
+};
