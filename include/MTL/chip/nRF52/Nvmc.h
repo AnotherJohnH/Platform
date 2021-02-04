41be91d87084092bc0c32aeec3fd33cb5c41231e
John D. Haughton <anotherjohnh@gmail.com>
2021-02-04 18:34:29 +0000

    micro::bit V2 enable I-cache

--- include/MTL/chip/nRF52/Nvmc.h
+++ include/MTL/chip/nRF52/Nvmc.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright (c) 2017 John D. Haughton
+// Copyright (c) 2021 John D. Haughton
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -19,17 +19,58 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //------------------------------------------------------------------------------
+//
+// NORDIC Semiconductor
+// nRF52
+// Non-volatile memory controller
+//
 
-#include "MTL/nRF52/Clock.h"
-#include "MTL/nRF52/Nvmc.h"
+#ifndef MTL_NRF52_NVMC_H
+#define MTL_NRF52_NVMC_H
 
-extern "C" {
+#include "MTL/Periph.h"
 
-void platform_init()
+namespace MTL {
+
+namespace nRF52 {
+
+union NvmcReg
 {
-   MTL::nRF52::Clock().startHFXO();
+   REG(0x400, ready);
+   REG(0x408, ready_next);
+   REG(0x504, config);
+   REG(0x508, erase_page);
+   REG(0x508, erase_pcr1);
+   REG(0x50C, erase_all);
+   REG(0x510, erase_pcr0);
+   REG(0x514, erase_uicr);
+   REG(0x518, erase_page_partial);
+   REG(0x51C, erase_page_partial_cfg);
+   REG(0x540, icache_cnf);
+   REG(0x548, ihit);
+   REG(0x54C, imiss);
+};
 
-   MTL::nRF52::Nvmc().enableICache();
-}
+class Nvmc : public Periph<NvmcReg,0x4001E000>
+{
+public:
+   Nvmc() = default;
 
-}
+   void enableICache(bool profile = false)
+   {
+      if (profile)
+         reg->icache_cnf = (1<<8) | (1<<0);
+      else
+         reg->icache_cnf = 1<<0;
+   }
+
+   uint32_t icacheHit() const { return reg->ihit; }
+
+   uint32_t icacheMiss() const { return reg->imiss; }
+};
+
+} // namespace nRF52
+
+} // namespace MTL
+
+#endif
