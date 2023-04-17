d4dba5db0a61fa051591e5531adfdd344cd4dbd5
John D. Haughton <anotherjohnh@gmail.com>
2023-04-17 21:23:18 +0100

    Add unsigned fixed point type

--- STB/UFixP.h
+++ STB/UFixP.h
@@ -26,18 +26,18 @@
 
 namespace STB {
 
-//! 32-bit signed fixed point type
+//! 32-bit unsigned fixed point type
 template <unsigned N>
-class FixP
+class UFixP
 {
 public:
-   using RAW = int32_t;
+   using RAW = uint32_t;
 
-   FixP() = default;
+   UFixP() = default;
 
    //! Construct from another fixed point value
    template <unsigned M>
-   FixP(FixP<M> that)
+   UFixP(UFixP<M> that)
    {
       if (M >= N)
          value = that.value >> (M - N);
@@ -47,81 +47,81 @@ public:
 
    //! Construct from another value
    template <typename TYPE>
-   FixP(TYPE value_) { value = UNITY * value_; }
+   UFixP(TYPE value_) { value = UNITY * value_; }
 
    //! Assign from another value
    template <typename TYPE>
-   FixP<N> operator=(TYPE that)
+   UFixP<N> operator=(TYPE that)
    {
-      value = FixP<N>(that).value;
+      value = UFixP<N>(that).value;
       return *this;
    }
 
    //! Add a value
    template <typename TYPE>
-   FixP<N> operator+=(TYPE that)
+   UFixP<N> operator+=(TYPE that)
    {
-      value += FixP<N>(that).value;
+      value += UFixP<N>(that).value;
       return *this;
    }
 
    //! Subtract a value
    template <typename TYPE>
-   FixP<N> operator-=(TYPE that)
+   UFixP<N> operator-=(TYPE that)
    {
-      value -= FixP<N>(that).value;
+      value -= UFixP<N>(that).value;
       return *this;
    }
 
    //! Multiply by an integer value
    //! XXX special case for better optimisation
-   FixP<N> operator*=(int8_t  that) { value *= that; return *this; }
-   FixP<N> operator*=(int16_t that) { value *= that; return *this; }
-   FixP<N> operator*=(int32_t that) { value *= that; return *this; }
-   FixP<N> operator*=(int64_t that) { value *= that; return *this; }
+   UFixP<N> operator*=(uint8_t  that) { value *= that; return *this; }
+   UFixP<N> operator*=(uint16_t that) { value *= that; return *this; }
+   UFixP<N> operator*=(uint32_t that) { value *= that; return *this; }
+   UFixP<N> operator*=(uint64_t that) { value *= that; return *this; }
 
    //! Multiply by a value
    template <typename TYPE>
-   FixP<N> operator*=(TYPE that)
+   UFixP<N> operator*=(TYPE that)
    {
       WIDE wide = value;
-      wide *= FixP<N>(that).value;
+      wide *= UFixP<N>(that).value;
       value = wide >> N;
       return *this;
    }
 
    //! Return addition with a value
    template <typename TYPE>
-   FixP<N> operator+(TYPE that) const
+   UFixP<N> operator+(TYPE that) const
    {
-      FixP<N> result {*this};
+      UFixP<N> result {*this};
       result += that;
       return result;
    }
 
    //! Return subtraction with a value
    template <typename TYPE>
-   FixP<N> operator-(TYPE that) const
+   UFixP<N> operator-(TYPE that) const
    {
-      FixP<N> result {*this};
+      UFixP<N> result {*this};
       result -= that;
       return result;
    }
 
    //! Return multiplication with a value
    template <typename TYPE>
-   FixP<N> operator*(TYPE that) const
+   UFixP<N> operator*(TYPE that) const
    {
-      FixP<N> result {*this};
+      UFixP<N> result {*this};
       result *= that;
       return result;
    }
 
-   // Cast to signed integer
-   operator int8_t()  const { return value >> FRAC_BITS; }
-   operator int16_t() const { return value >> FRAC_BITS; }
-   operator int32_t() const { return value >> FRAC_BITS; }
-   operator int64_t() const { return value >> FRAC_BITS; }
+   // Cast to unsigned integer
+   operator uint8_t()  const { return value >> FRAC_BITS; }
+   operator uint16_t() const { return value >> FRAC_BITS; }
+   operator uint32_t() const { return value >> FRAC_BITS; }
+   operator uint64_t() const { return value >> FRAC_BITS; }
 
    // Cast to floating point
    operator float()  const { return float(value) / UNITY; }
@@ -129,22 +129,22 @@ public:
 
    // Comparison operators
    template <typename TYPE>
-   bool operator<(TYPE that) const { return value < FixP<N>(that).value; }
+   bool operator<(TYPE that) const { return value < UFixP<N>(that).value; }
 
    template <typename TYPE>
-   bool operator<=(TYPE that) const { return value <= FixP<N>(that).value; }
+   bool operator<=(TYPE that) const { return value <= UFixP<N>(that).value; }
 
    template <typename TYPE>
-   bool operator>(TYPE that) const { return value > FixP<N>(that).value; }
+   bool operator>(TYPE that) const { return value > UFixP<N>(that).value; }
 
    template <typename TYPE>
-   bool operator>=(TYPE that) const { return value >= FixP<N>(that).value; }
+   bool operator>=(TYPE that) const { return value >= UFixP<N>(that).value; }
 
    template <typename TYPE>
-   bool operator==(TYPE that) const { return value == FixP<N>(that).value; }
+   bool operator==(TYPE that) const { return value == UFixP<N>(that).value; }
 
    template <typename TYPE>
-   bool operator!=(TYPE that) const { return value != FixP<N>(that).value; }
+   bool operator!=(TYPE that) const { return value != UFixP<N>(that).value; }
 
    //! Return raw value
    RAW raw() const { return value; }
@@ -156,18 +156,18 @@ public:
       return value & MASK;
    }
 
-   //! Translate raw value into FixP value
-   static FixP<N> fromRaw(RAW raw)
+   //! Translate raw value into UFixP value
+   static UFixP<N> fromRaw(RAW raw)
    {
-      FixP<N> result {};
+      UFixP<N> result {};
       result.value = raw;
       return result;
    }
 
    //! Return smallest representable positive value
-   static FixP<N> DELTA()
+   static UFixP<N> DELTA()
    {
-      FixP<N> result {};
+      UFixP<N> result {};
       result.value = 1;
       return result;
    }
@@ -181,7 +181,7 @@ public:
    static const unsigned FRAC_BITS = N;
 
 protected:
-   using WIDE = int64_t;
+   using WIDE = uint64_t;
 
    static const unsigned BITS  = sizeof(RAW) * 8;
    static const WIDE     UNITY = WIDE(1) << FRAC_BITS;
