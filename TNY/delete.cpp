d67e8c19c6953fca4873cd1fda5b618f71a4050b
John D. Haughton <anotherjohnh@gmail.com>
2024-01-20 18:42:12 +0000

    Add sized delete operator stub

--- TNY/delete.cpp
+++ TNY/delete.cpp
@@ -20,9 +20,16 @@
 // SOFTWARE.
 //------------------------------------------------------------------------------
 
-//! \brief tiny C library implementation
+//! \brief tiny C/C++ library implementation
 
-void operator delete(void *)
-{
-   // Nothing to do dynamic memory management is not expected
-}
+#include <cstddef>
+
+// XXX gcc is bringing in a dependency on the delete operator
+//     when a virtual destructor is used. This is not expected
+//     (by me) and have not dug into why it's there
+
+// Nothing to do dynamic memory management is not expected
+
+void operator delete(void*) {}
+void operator delete(void*, size_t) {}
+
