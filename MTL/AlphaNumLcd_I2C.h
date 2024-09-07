e9e25496735e9bb40e92a8b0951cd3d5fa0138cd
John D. Haughton <anotherjohnh@gmail.com>
2024-09-07 23:10:04 +0100

    Complete and fix AlphaNumLCD_I2C

--- MTL/AlphaNumLcd_I2C.h
+++ MTL/AlphaNumLcd_I2C.h
@@ -20,44 +20,30 @@
 // SOFTWARE.
 //------------------------------------------------------------------------------
 
-// \brief HD44780 compatible alphanumeric LCD support via I2C
-
-// XXX not working needs completion and testing XXX
+// \brief I2C AIP31068L or compatible alphanumeric LCD
 
 #pragma once
 
-#include "MTL/rp2040/I2C.h"
-
 #include <unistd.h>
+
+#include "MTL/MTL.h"
 
 namespace MTL {
 
-template <typename TYPE_I2C, unsigned COLS, unsigned ROWS>
+template <typename I2C, unsigned COLS, unsigned ROWS>
 class AlphaNumLcd
 {
 public:
-   AlphaNumLcd()
+   AlphaNumLcd(uint8_t i2c_addr_ = 0x3E)
    {
-      usleep(40000);
+      i2c.setTarget(i2c_addr_);
 
-      // Initialisation involves sending the "function" cmd four times
-      function();
-      usleep(4100);
+      usleep(15000);
 
       function();
-      usleep(100);
-
-      function();
-      usleep(100);
-
-      function();
-
-      control(/* on */ false, /* cursor */ false, /* blink */ false);
-      control(/* on */ true,  /* cursor */ false, /* blink */ false);
-
-      entryMode(/* inc */ true, /* shift */ false);
-
+      display(/* on */ true,  /* cursor */ false, /* blink */ false);
       clear();
+      entryMode(/* inc */ true, /* shift */ false);
    }
 
    //! Clear display and home
@@ -65,8 +51,8 @@ public:
    {
       col = row = 0;
 
-      send(0b00000001);
-      usleep(1520);
+      sendCmd(0b00000001);
+      usleep(1530);
    }
 
    //! Move cursor to home
@@ -74,8 +60,8 @@ public:
    {
       col = row = 0;
 
-      send(0b00000010);
-      usleep(1520);
+      sendCmd(0b00000010);
+      usleep(1530);
    }
 
    //! Set entry mode
@@ -86,11 +72,11 @@ public:
       if (inc)   cmd |= 0b10;
       if (shift) cmd |= 0b01;
 
-      send(cmd);
+      sendCmd(cmd);
    }
 
    //! Display and cusror control
-   void control(bool on, bool cursor, bool blink)
+   void display(bool on, bool cursor, bool blink)
    {
       uint8_t cmd = 0b00001000;
 
@@ -98,16 +84,18 @@ public:
       if (cursor) cmd |= 0b010;
       if (blink)  cmd |= 0b001;
 
-      send(cmd);
+      sendCmd(cmd);
    }
 
    //! Move cursor
    void move(unsigned col_, unsigned row_)
    {
+      uint8_t cmd = 0b10000000;
+
       col = col_ < COLS ? col_ : 0;
       row = row_ < ROWS ? row_ : 0;
 
-      send(0b10000000 | (row << 6) | col);
+      sendCmd(cmd | (row << 6) | col);
    }
 
    //! Send single char to the display
@@ -115,7 +103,7 @@ public:
    {
       if (ch != '\n')
       {
-         send(ch, /* ram */ true);
+         sendRam(ch, true);
       }
 
       if ((col++ == COLS) || (ch == '\n'))
@@ -143,19 +131,29 @@ public:
    //! Program a character
    void progChar(uint8_t ch, const uint8_t* bitmap)
    {
-      send(0b01000000 | ((ch & 0b111) << 3));
+      sendCmd(0b01000000 | ((ch & 0b111) << 3));
 
       for(unsigned r = 0; r < 8; ++r)
-         send(bitmap[r]);
+         sendRam(bitmap[r], r == 7);
 
       move(col, row);
    }
 
 private:
    //! Send command to LCD
-   void send(uint8_t data, bool ram = false)
+   void sendRam(uint8_t data, bool stop)
    {
-       /// i2c.write();
+       i2c.write(0x40, false); // No further command (b7) and RAM (b6)
+       i2c.write(data, stop);
+       usleep(100);
+   }
+
+   //! Send command to LCD
+   void sendCmd(uint8_t data)
+   {
+       i2c.write(0x00, false); // No further command (b7) and not RAM (b6)
+       i2c.write(data, true);
+       usleep(100);
    }
 
    //! Initial function control
@@ -167,12 +165,12 @@ private:
       if (ROWS == 2) cmd |= 0b01000;
       if (font)      cmd |= 0b00100;
 
-      send(cmd);
+      sendCmd(cmd);
    }
 
-   TYPE_I2C i2c;
-   uint8_t  col;
-   uint8_t  row;
+   I2C     i2c{};
+   uint8_t col{0};
+   uint8_t row{0};
 };
 
 } // namespace MTL
