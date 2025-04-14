bb61b8047f148d4a0ce173d7341499e1ac54e461
John D. Haughton <anotherjohnh@gmail.com>
2025-04-14 18:17:55 +0100

    Minor tidy of rp2xxxx PioI2S_S16

--- MTL/chip/rp2xxx/PioI2S_S16.h
+++ MTL/chip/rp2xxx/PioI2S_S16.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright (c) 2023 John D. Haughton
+// Copyright (c) 2025 John D. Haughton
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -44,34 +44,34 @@ public:
       // OSR = R:L
       lbl(left_loop);
       //---------------------------------------------------------------
-         OUT(PIO::PINS, 1)                .side(S_0_0);
+         OUT(PIO::PINS, 1)                .side(0b00);
       //---------------------------------------------------------------
-         JMP(PIO::X_NE_Z_DEC, left_loop)  .side(S_0_1);
+         JMP(PIO::X_NE_Z_DEC, left_loop)  .side(0b01);
       //---------------------------------------------------------------
-         OUT(PIO::PINS, 1)                .side(S_1_0);
+         OUT(PIO::PINS, 1)                .side(0b10);
       //---------------------------------------------------------------
-         SET(PIO::X, X_INIT)              .side(S_1_1);
+         SET(PIO::X, X_INIT)              .side(0b11);
       //---------------------------------------------------------------
 
       //###############################################################
       // OSR = 0:R
       lbl(right_loop);
       //---------------------------------------------------------------
-         OUT(PIO::PINS, 1)                .side(S_1_0);
+         OUT(PIO::PINS, 1)                .side(0b10);
       //---------------------------------------------------------------
-         JMP(PIO::X_NE_Z_DEC, right_loop) .side(S_1_1);
+         JMP(PIO::X_NE_Z_DEC, right_loop) .side(0b11);
       //---------------------------------------------------------------
-         OUT(PIO::PINS, 1)                .side(S_0_0);
+         OUT(PIO::PINS, 1)                .side(0b00);
       //---------------------------------------------------------------
-         SET(PIO::X, X_INIT)              .side(S_0_1);
+         SET(PIO::X, X_INIT)              .side(0b01);
       //---------------------------------------------------------------
 
       wrap();
    }
 
-   signed download(unsigned clock_freq,
-                   unsigned pin_sd,
-                   unsigned pin_lrclk_sclk)
+   signed download(unsigned clock_freq_,
+                   unsigned pin_sd_,
+                   unsigned pin_sclk_lrclk_)
    {
       // Allocate a state machine
       sd = pio.allocSM();
@@ -83,14 +83,14 @@ public:
          return -1;
 
       // Configure state machine
-      pio.SM_clock(    sd, clock_freq);
-      pio.SM_pinOUT(   sd, pin_sd);
-      pio.SM_pinSIDE(  sd, pin_lrclk_sclk);
+      pio.SM_clock(    sd, clock_freq_);
+      pio.SM_pinOUT(   sd, pin_sd_);
+      pio.SM_pinSIDE(  sd, pin_sclk_lrclk_);
       pio.SM_configOSR(sd, I2S_BITS_PER_SAMPLE * 2, MTL::SHIFT_LEFT,
                        MTL::AUTO_PULL, /* join_tx */ true);
 
       unsigned x_init = X_INIT - 2;
-      pio.SM_exec(sd, SET(PIO::X, x_init).side(S_0_0).op() );
+      pio.SM_exec(sd, SET(PIO::X, x_init).side(0b00).op() );
 
       return sd;
    }
@@ -100,11 +100,13 @@ public:
       pio.start(1 << sd);
    }
 
+   //! Tx a packed 16-bit sample pair
    void push(uint32_t packed_)
    {
       pio.SM_push(sd, packed_);
    }
 
+   //! Tx a 16-bit sample pair
    void push(int16_t left_, int16_t right_)
    {
       uint32_t packed = (left_ << 16) | (right_ & 0xFFFF);
@@ -115,11 +117,6 @@ public:
 private:
    static const unsigned I2S_BITS_PER_SAMPLE = 16;
    static const unsigned X_INIT              = I2S_BITS_PER_SAMPLE - 2;
-
-   static const uint8_t S_0_0{0b00};
-   static const uint8_t S_0_1{0b01};
-   static const uint8_t S_1_0{0b10};
-   static const uint8_t S_1_1{0b11};
 
    PIO_TYPE pio{};
    int      sd{-1};
