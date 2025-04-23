90c2cf944bd18fab60e01c67191ad012f4d94832
John D. Haughton <anotherjohnh@gmail.com>
2025-04-23 19:01:08 +0100

    XmlFile allow pre suffixing filkenames with .xml

--- STB/XmlFile.h
+++ STB/XmlFile.h
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------------------
-// Copyright (c) 2012-2019 John D. Haughton
+// Copyright (c) 2012 John D. Haughton
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,8 +20,7 @@
 // SOFTWARE.
 //------------------------------------------------------------------------------
 
-#ifndef XML_FILE_H
-#define XML_FILE_H
+#pragma once
 
 #include <cstdio>
 
@@ -43,10 +42,11 @@ public:
    virtual std::string getName() const = 0;
 
    //! Construct an object from an XML file
-   static TYPE* read(const std::string& name)
+   static TYPE* read(const std::string& name_)
    {
-      std::string filename = name;
-      filename += ".xml";
+      std::string filename = name_;
+
+      ensureSuffix(filename);
 
       XML::Document xml(filename, /* require_prolog */ false);
 
@@ -57,7 +57,8 @@ public:
    void write() const
    {
       std::string filename = getName();
-      filename += ".xml";
+
+      ensureSuffix(filename);
 
       FILE* fp = fopen(filename.c_str(), "w");
       if (fp != nullptr)
@@ -69,8 +70,16 @@ public:
          fclose(fp);
       }
    }
+
+   //! Add .xml to filename if missing
+   static void ensureSuffix(std::string& filename_)
+   {
+      if ((filename_.size() < 4) ||
+          (filename_.compare(filename_.size() - 4, 4, ".xml") != 0))
+      {
+         filename_ += ".xml";
+      }
+   }
 };
 
 } // namespace XML
-
-#endif
