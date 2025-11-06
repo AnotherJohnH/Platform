c916250d280b33299b6fe4e6739d7fec1539493c
John D. Haughton <anotherjohnh@gmail.com>
2025-11-06 19:23:50 +0000

    Add text LCD font

---
+++ GUI/Font/Lcd.h
@@ -0,0 +1,14 @@
+//-------------------------------------------------------------------------------
+// Copyright (c) 2025 John D. Haughton
+// SPDX-License-Identifier: MIT
+//-------------------------------------------------------------------------------
+
+#pragma once
+
+#include "GUI/Font.h"
+
+namespace GUI {
+
+extern const Font font_lcd;
+
+} // namespace GUI
