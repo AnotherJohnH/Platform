65ecf92522d57e9083926d37bf7524907484077b
John D. Haughton <sloecomputer@gmail.com>
2026-03-08 16:50:22 +0000

    Some fixes for picoGame hardware

--- Hardware/picoGame/Config.h
+++ Hardware/picoGame/Config.h
@@ -21,7 +21,8 @@
 #define HW_USB_FILE_DEVICE
 #define HW_DISPLAY_TUFTY2040
 #define HW_LED            MTL::tufty2040::PIN_USER_LED
-#define HW_BUTTONS_TUFTY2040
+#define HW_BUTTONS      5
+#define HW_BUTTONS_PIN  MTL::tufty2040::PIN_SW_DN
 
 //------------------------------------------------------------------------------
 #elif defined(HW_NATIVE)
@@ -33,7 +34,7 @@
 #define HW_DISPLAY_WIDTH 320
 #define HW_DISPLAY_HEIGHT 256
 #define HW_LED_NATIVE
-#define HW_BUTTONS_NATIVE
+#define HW_BUTTONS      5
 
 //------------------------------------------------------------------------------
 #else
