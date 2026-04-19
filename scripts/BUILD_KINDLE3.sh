5e5a9f24498bf3a58ba464ceb379e38909ba69d1
John D. Haughton <sloecomputer@gmail.com>
2026-04-19 21:02:24 +0100

    Some cosmetic tidy

--- scripts/BUILD_KINDLE3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/echo "source this file then type 'scons' to run SConstruct"
-#
-# Setup environment for Kindle3 build
-
-export PROJ_TARGET=Kindle3
-
--- scripts/BUILD_NATIVE.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/echo "source this file then type 'scons' to run SConstruct"
-#
-# Setup environment for a native (host is target) build
-
-unset PROJ_TARGET
-
