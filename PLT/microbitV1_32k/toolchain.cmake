11ea73a75c5e758a8203c308f51e0bea8d9e996e
John D. Haughton <sloecomputer@gmail.com>
2026-04-26 21:39:16 +0100

    More tidy up

--- PLT/microbitV1_32k/toolchain.cmake
+++
@@ -1,8 +0,0 @@
-#-------------------------------------------------------------------------------
-# Copyright (c) 2020 John D. Haughton
-# SPDX-License-Identifier: MIT
-#-------------------------------------------------------------------------------
-
-set(PDK_RAM_SIZE 32k)
-
-include(${CMAKE_SOURCE_DIR}/Platform/PLT/microbit/toolchain.cmake)
