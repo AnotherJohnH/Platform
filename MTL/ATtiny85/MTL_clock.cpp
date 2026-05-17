eb76c6594d73dbe086211e29d1b7174272386904
John D. Haughton <sloecomputer@gmail.com>
2026-05-17 14:39:44 +0100

    Some ATtiny build fixes

---
+++ MTL/ATtiny85/MTL_clock.cpp
@@ -0,0 +1,18 @@
+//-------------------------------------------------------------------------------
+// Copyright (c) 2023 John D. Haughton
+// SPDX-License-Identifier: MIT
+//-------------------------------------------------------------------------------
+
+#include "MTL/MTL.h"
+
+//! Return 100 Hz clock value
+uint32_t MTL_clock()
+{
+   return 0;
+}
+
+//! Return 1 MHz clock value
+uint32_t MTL_us_clock()
+{
+   return 0;
+}
